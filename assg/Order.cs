82ec906180f36de04985847f75a43f721ccaf685
Jia En Sophie Ang <s10258570@connect.np.edu.sg>
2024-01-09 23:25:54 +0800

    idk

--- assg/Order.cs
+++ assg/Order.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace assg
 {
-    internal class Order
+    class Order
     {
         public int Id { get; set; }
 
         public DateTime timeRecieved { get; set; }
+
         public DateTime? timeFulffilled { get; set; }
         public List<IceCream> iceCreamList { get; set; } = new List<IceCream>();
+
+
         public Order() { }
         public Order(int i, DateTime tr)
         {
@@ -38,7 +41,11 @@ namespace assg
         }
         public override string ToString()
         {
+<<<<<<< HEAD
             return $"{Id}\t{timeRecieved}\t{timeFulffilled}";
+=======
+            return $"{Id}\t{timeRecieved}\t{timeFulfilled}";
+>>>>>>> 766ec3c4bd1dd2936aa1653bcc53ee7555f99c5d
         }
     }
 }

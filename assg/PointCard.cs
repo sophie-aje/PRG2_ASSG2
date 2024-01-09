1f9986fd97ba6f7c9469606d151cc7430f74f28d
Jia En Sophie Ang <s10258570@connect.np.edu.sg>
2024-01-09 22:50:53 +0800

    updated orderclass

--- assg/PointCard.cs
+++ assg/PointCard.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PRG_Assignment
+namespace assg
 {
-    internal class PointCard
+    class PointCard
     {
+        public int points { get; set; }
+        public int punchCard { get; set; }
+        public string tier { get; set; }
+        public PointCard() { }
+        public PointCard(int Points, int PunchCard)
+        {
+            points = Points;
+            punchCard = PunchCard;
+        }
+        public void AddPoints(int add)
+        {
+            points = points + add;
+        }
+
+        public void RedeemPoints(int redeem)
+        {
+            points = points + redeem;
+        }
+
+        public void Punch() { }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
     }
 
 }

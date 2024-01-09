3d6ae576805710c63f3c0e55caaca2b5ca079f00
Nehaa Nayaana Amar Raj <s10256085@connect.np.edu.sg>
2024-01-09 22:12:02 +0800

    updated cup class

---
+++ assg/Cup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	class Cup : IceCream
+	{
+		public Cup() { }
+        public Cup(string o, int s, List<Flavour> f, List<Topping> t)
+        {
+            option = o;
+            scoops = s;
+            flavours = f;
+            toppings = t;
+        }
+    }
+}

362a2c2616618e7ad900760f1ceb520bf3d55f48
Nehaa Nayaana Amar Raj <s10256085@connect.np.edu.sg>
2024-01-09 22:02:58 +0800

    updated PointCard class

--- assg/PointCard.cs
+++ assg/PointCard.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace PRG_Assignment
 {
-    internal class PointCard
+    class PointCard
     {
+        public int points { get; set; }
+        public int punchCard { get; set; }
+        public string tier { get; set; }
+        public PointCard() { }
+        public PointCard(int Points, int PunchCard)
+        {
+            points = Points;
+            punchCard = PunchCard;
+        }
+        public void AddPoints(int add)
+        {
+            points = points + add;
+        }
+
+        public void RedeemPoints(int redeem)
+        {
+            points = points + redeem;
+        }
+
+        public void Punch() { }
+
+        public override string ToString()
+        {
+            return "";
+        }
     }
+
 }

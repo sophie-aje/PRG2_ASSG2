766ec3c4bd1dd2936aa1653bcc53ee7555f99c5d
Nehaa Nayaana Amar Raj <s10256085@connect.np.edu.sg>
2024-01-09 23:18:32 +0800

    updated order class

--- assg/Order.cs
+++ assg/Order.cs
@@ -40,7 +40,7 @@ namespace assg
         }
         public override string ToString()
         {
-            return $"{Id}\t{timeRecieved}\t{timeFuffilled}";
+            return $"{Id}\t{timeRecieved}\t{timeFulfilled}";
         }
     }
 }

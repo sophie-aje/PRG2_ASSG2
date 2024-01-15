ab398458e31f26fe0c0d1689d813324add5ff91a
Nehaa Nayaana Amar Raj <s10256085@connect.np.edu.sg>
2024-01-15 12:10:04 +0800

    updated porgam

--- assg/Program.cs
+++ assg/Program.cs
@@ -18,19 +18,20 @@ void DisplayMenu()
             Console.WriteLine("[4] Create a customers' order");
             Console.WriteLine("[5] Display order details of a customer");
             Console.WriteLine("[6] Modify order details");
-            Console.Write("Enter an option: ");
+            Console.Write("\nEnter an option: ");
             int option = Convert.ToInt32(Console.ReadLine());
             if (option == 1)
             {
+                Option1();
 
             }
             else if (option == 2)
             {
-
+                
             }
             else if (option == 3)
             {
-
+                
             }
             else if (option == 4)
             {
@@ -66,11 +67,29 @@ void DisplayMenu()
 DisplayMenu();
 
 //---BASIC FEATURES---
+
 //Option 1: 
 void Option1()
 {
-
-
+    //reading from customers.csv file
+    using (StreamReader sr = new StreamReader("customers.csv"))
+    {
+        string? s = sr.ReadLine(); // read the heading
+                                   // display the heading
+        if (s != null)
+        {
+            string[] heading = s.Split(',');
+            Console.WriteLine("{0,10}  {1,10}  {2,10}  {3,10}",
+                heading[0], heading[1], heading[2], heaing[3], heading[4], heading[5]);
+            // repeat until end of file
+        }
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] info = s.Split(',');
+            Console.WriteLine("{0,10}  {1,10}  {2,10}  {3,10}",
+                info[0], info[1], info[2], info[3], info[4], info[5]);
+        }
+    }
 }
 
 //Option 2: 
